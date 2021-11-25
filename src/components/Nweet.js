01a1e70ed0adefeec5863e2fc0a9036e3991f1c5
JY-0610 <jjyyyy0610@naver.com>
2021-11-25 20:00:29 +0900

    1.0 Initialization

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,20 +1,24 @@
-import { dbService } from "fbase";
+import { dbService,storageService } from "fbase";
 import { useState } from "react";
 
 const Nweet =({nweetObj , isOwner}) => {
     const [editing,setEditing] = useState(false);
     const [newNweet,setNewNweet] = useState(nweetObj.text);
+
+
     const onDeleteClick =async () =>{
         const ok = window.confirm("삭제하시겠습니까?")
-        console.log(ok);
+      
+        
         if(ok){
-            console.log(nweetObj.id);
-            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
-            console.log(data);
+           
+             await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            if (nweetObj.attachmentUrl !=="")
+            await storageService.refFromURL(nweetObj.attachmentUrl).delete();
         }
     };
 
-    const toggleEditing = () => setEditing((prev) =>!prev);
+    const toggleEditing = () => setEditing((prev) => !prev);
 
     const onChange = (event) => {
         const {
@@ -40,6 +44,9 @@ const Nweet =({nweetObj , isOwner}) => {
         ):(
             <>
             <h4>{nweetObj.text}</h4>
+            {nweetObj.attachmentUrl &&(
+                <img src ={nweetObj.attachmentUrl} width="50px" height="50px" />
+            )}
             {isOwner && (
                 <>
                 <button onClick ={onDeleteClick}>Delete Nweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useEffect,useState } from "react";
 import  Nweet from "components/Nweet";
+import {v4 as uuidv4} from "uuid";
 
 const Home=({userObj})=>{
     
@@ -23,15 +24,29 @@ const Home=({userObj})=>{
   
 
     const onSubmit = async(event) => {
-        
+
         event.preventDefault();
+        
+        let attachmentUrl ="";
+        if(attachment !==""){
+        const attachmentRef = storageService
+        .ref()
+        .child(`${userObj.uid}/${uuidv4()}`);
+        const response = await attachmentRef.putString(attachment,"data_url");
+        attachmentUrl = await response.ref.getDownloadURL();
+    }
         await dbService.collection("nweets").add({
-            text:nweet,
-            createAt:Date.now(),
-            creatorId:userObj.uid,
+            Text:nweet,
+            createdAt: Date.now(),
+            creatorId :userObj.uid,
+            attachmentUrl
         });
         setNweet("");
-    };
+        setAttachment("");
+        // console.log(await response.ref.getDownloadURL());
+        // storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+
+    }
     const onChange = (event) => {
         event.preventDefault();
         const {
@@ -42,7 +57,7 @@ const Home=({userObj})=>{
 const onFileChange =(event) => {
    const {
        target: {files},
-   }=event;
+   } = event;
    const theFile = files[0];
    const reader = new FileReader();
    reader.onloadend = (finishedEvent) =>{
@@ -50,7 +65,7 @@ const onFileChange =(event) => {
            currentTarget:{result},
        } =finishedEvent;
        setAttachment(result);
-}
+};
    reader.readAsDataURL(theFile);
 
    
