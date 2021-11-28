3820b38b867c567d767f82381d0deb39e69f1b5e
JY-0610 <jjyyyy0610@naver.com>
2021-11-28 11:59:20 +0900

    1.0 Initailization

--- src/components/Router.js
+++ src/components/Router.js
@@ -10,7 +10,16 @@ const AppRouter=({isLoggedIn, userObj,refreshUser})=>{
             {isLoggedIn && <Navigation userObj={userObj} />}
           <Switch>
                 {isLoggedIn ? (
-                    <>
+                    <div
+                    style={{
+                        maxWidth:890,
+                        width:"100%",
+                        margin:"0 auto",
+                        marginTop:80,
+                        display:"flex",
+                        justifyContent:"center"
+                    }}
+                    >
                     <Route exact path="/" >
                         <Home userObj ={userObj} />
                         </Route>
@@ -18,14 +27,13 @@ const AppRouter=({isLoggedIn, userObj,refreshUser})=>{
                         <Route  exact path ="/profile">
                             <Profile refreshUser={refreshUser} userObj={userObj} />
                             </Route>
-                            </>
+                            </div>
                 ) : (
                     <Route exact path="/" >
                         <Auth />
                         </Route>
 
 )}
-
       </Switch>
         </Router>
     );
